78b4e2bdf34aaeb2a66f9e5ecd168dc317b25962
eason <lolmuta@gmail.com>
2024-03-05 01:36:59 +0800

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,204 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Shop.Models;
+using Shop.Utils;
+using System.Data;
+using System.Diagnostics;
+
+namespace Shop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly DbHelper dbHelper;
+        private readonly UserInfoHelper userInfoHelper;
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public HomeController(ILogger<HomeController> logger
+            , DbHelper dbHelper
+            , UserInfoHelper userInfoHelper
+            , IHttpContextAccessor _httpContextAccessor)
+        {
+            _logger = logger;
+            this.dbHelper = dbHelper;
+            this.userInfoHelper = userInfoHelper;
+            httpContextAccessor = _httpContextAccessor;
+        }
+
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+        [HttpGet]
+        public IActionResult IndexQuery()
+        { 
+            var dt = dbHelper.Query(@"
+                 SELECT [IDNo]
+                      ,[name]
+                      ,[count]
+                      ,[src]
+                      , price  
+                  FROM [TestDB].[dbo].[Items]", null);
+
+            var jsonData = dt.AsEnumerable().Select(x => new
+            {
+                IDNo = x.Field<int>("IDNo"),
+                name = x.IsNull("name") ? "" : x.Field<string>("name"),
+                count = x.IsNull("count") ? 0 : x.Field<int>("count"),
+                src = x.IsNull("src") ? "" : x.Field<string>("src"),
+                price = x.IsNull("price") ? 0m : x.Field<decimal>("price"),
+            });
+
+            return Json(jsonData);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Item(string id)
+        {
+            var request = httpContextAccessor.HttpContext.Request;
+            var baseUrl = $"{request.Scheme}://{request.Host}";
+
+            //string id = HttpContext.Request.Query["id"];
+            var ItemInfo = dbHelper.Query<ItemInfo>(@"
+                 SELECT [IDNo]
+                      ,[name]
+                      ,[count]
+                      , @baseUrl + '/' + [src] as src
+                      , price
+                  FROM [TestDB].[dbo].[Items]
+                  where IDNo = @IDNo", 
+                  p => 
+                  { 
+                      p.AddWithValue("@IDNo", id);
+                      p.AddWithValue("@baseUrl", baseUrl);
+                  })
+                .First();
+            
+                return View(ItemInfo);
+        }
+        [HttpPost]
+        public IActionResult UpdateBuyItem([FromBody] BuyInfo buyInfo)
+        {
+            try
+            {
+                var sql = @" 
+                MERGE [dbo].[BuyItems] AS tgt
+                USING (SELECT @BuyCount as BuyCount, @Items_IDNo as Items_IDNo, @UserId as UserId) AS src
+                    ON (tgt.Items_IDNo = src.Items_IDNo and tgt.UserID = src.UserId)
+                WHEN MATCHED
+                    THEN
+                        UPDATE
+                        SET tgt.buyCount = src.buyCount
+				
+                WHEN NOT MATCHED BY Target 
+                    THEN
+                        INSERT (buyCount, Items_IDNo, UserId)
+                        VALUES (src.buyCount, src.Items_IDNo, src.UserId);
+                ";
+                var UserId = userInfoHelper.GetUserId();
+                Action<SqlParameterCollection> paramAct = sqlCmd =>
+                {
+                    sqlCmd.AddWithValue("@BuyCount", buyInfo.buyCount);
+                    sqlCmd.AddWithValue("@Items_IDNo", buyInfo.IDNo);
+                    sqlCmd.AddWithValue("@UserId", UserId);
+                };
+                var result = dbHelper.ExecuteNonQuery(sql, paramAct);
+                if (result == 1)
+                {
+                    // Return success message
+                    return Ok(new { success = true, message = "Item updated successfully" });
+                }
+                else
+                {
+                    // Return error message
+                    return BadRequest(new { success = false, message = "Failed to update item" });
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "測試");
+                return BadRequest(new { success = false, message = "exception " + ex.Message });
+            }
+        }
+        [HttpPost]
+        public IActionResult DeleteBuyItem([FromBody] BuyInfoDelete buyInfoDelete)
+        {
+            try
+            {
+                var sql = @" 
+                    delete [BuyItems] where IDNo=@idNo;
+                ";
+                var UserId = userInfoHelper.GetUserId();
+                Action<SqlParameterCollection> paramAct = sqlCmd =>
+                {
+                    sqlCmd.AddWithValue("@idNo", buyInfoDelete.IDNo);
+                };
+                var result = dbHelper.ExecuteNonQuery(sql, paramAct);
+                if (result == 1)
+                {
+                    // Return success message
+                    return Ok(new { success = true, message = "Item delete successfully" });
+                }
+                else
+                {
+                    // Return error message
+                    return BadRequest(new { success = false, message = "Failed to delete item" });
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "測試");
+                return BadRequest(new { success = false, message = "exception " + ex.Message });
+            }
+        }
+        public IActionResult BuyItemList()
+        {
+            var request = httpContextAccessor.HttpContext.Request;
+            var baseUrl = $"{request.Scheme}://{request.Host}";
+            var sql = @"
+            SELECT [BuyItems].[IDNo]
+                  ,[BuyItems].[BuyCount]
+                  ,[BuyItems].[Items_IDNo]
+	              ,[Items].Name
+	              ,[Items].price
+                  ,@baseUrl + '/' + [Items].src as src
+            FROM [TestDB].[dbo].[BuyItems]
+            left join [dbo].[Items] on [Items].IDNo = [BuyItems].[Items_IDNo]
+            where 1 = 1
+	        and [BuyItems].UserId = @UserId
+            ";
+            var UserId = userInfoHelper.GetUserId();
+            var list = dbHelper.Query<BuyItem>(sql, sqlcmd =>
+            {
+                sqlcmd.AddWithValue("@UserId", UserId);
+                sqlcmd.AddWithValue("@baseUrl", baseUrl);
+            });
+
+            return View(list);
+        }
+        public IActionResult Buy()
+        {
+            return View();
+        }
+    }
+    public class BuyInfo
+    {
+        public int IDNo { get; set; }
+        public int buyCount { get; set; }
+    }
+    public class BuyInfoDelete
+    {
+        public int IDNo { get; set; }
+    }
+}
---
+++ Utils/DbHelper.cs
@@ -0,0 +1,211 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using WinformTable.Ext;
+using System.Reflection;
+
+namespace Shop.Utils
+{
+    public class DbHelper
+    {
+        private readonly IConfiguration _configuration;
+        
+        public DbHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public T ConnDb<T>(Func<SqlConnection, T> func)
+        {
+            var connectionString = _configuration.GetConnectionString("Database");
+            using (var sqlconn = new SqlConnection(connectionString))
+            {
+                sqlconn.Open();
+                return func(sqlconn);
+            }
+        }
+        public void ConnDb(Action<SqlConnection> act)
+        {
+            ConnDb(act.ToFunc());
+        }
+        public DataTable Query(string sqlQuery, Action<SqlParameterCollection> paramAct)
+        {
+            DataTable dataTable = new DataTable();
+            var connectionString = _configuration.GetConnectionString("Database");
+            using (var sqlconn = new SqlConnection(connectionString))
+            {
+                sqlconn.Open();
+                using (SqlCommand command = new SqlCommand(sqlQuery, sqlconn))
+                {
+                    paramAct?.Invoke(command.Parameters);
+                    // 创建一个 SqlDataAdapter 并使用它填充 DataTable
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                }
+            }
+            return dataTable;
+        }
+        public List<T> Query<T>(string sqlQuery, Action<SqlParameterCollection> paramAct) where T : new()
+        {
+            DataTable dataTable = Query(sqlQuery, paramAct);
+           
+            return ConvertToList<T>(dataTable);
+        }
+        public T ExecuteScalar<T>(string sqlQuery, Action<SqlParameterCollection> paramAct)
+        {
+            var connectionString = _configuration.GetConnectionString("Database");
+            using (var sqlconn = new SqlConnection(connectionString))
+            {
+                sqlconn.Open();
+                using (SqlCommand command = new SqlCommand(sqlQuery, sqlconn))
+                {
+                    paramAct?.Invoke(command.Parameters);
+                    // 执行查询并返回结果
+                    object result = command.ExecuteScalar();
+                    if (result != null && result != DBNull.Value)
+                    {
+                        return (T)Convert.ChangeType(result, typeof(T));
+                    }
+                    else
+                    {
+                        // 如果结果为 null，则返回默认值
+                        return default(T);
+                    }
+                }
+            }
+        }
+        public int ExecuteNonQuery(string sqlExecute, Action<SqlParameterCollection> paramAct)
+        {
+            var connectionString = _configuration.GetConnectionString("Database");
+            using (var sqlconn = new SqlConnection(connectionString))
+            {
+                sqlconn.Open();
+                using (SqlCommand command = new SqlCommand(sqlExecute, sqlconn))
+                {
+                    paramAct?.Invoke(command.Parameters);
+                    // 创建一个 SqlDataAdapter 并使用它填充 DataTable
+                    return command.ExecuteNonQuery();
+                }
+            }
+        }
+        public string GetPageSql(string sqlQuery, int PageSize, int PageNumber)
+        {
+            int start = (PageNumber - 1) * PageSize + 1;
+            int end = start + PageSize;
+            var sqlPageQuery = $@"
+                select a.* from ({sqlQuery}) a 
+                where a.RowNum>={start} and a.RowNum<{end}";
+            return sqlPageQuery;
+        }
+        public int GetTotalPage(string sqlQuery, Action<SqlParameterCollection> paramAct, int PageSize)
+        {
+            var sqlDataCount = $@"select count(0) from ({sqlQuery}) a ";
+            var dataCount = ExecuteScalar<int>(sqlDataCount, paramAct);
+            var totalPage = dataCount / PageSize;
+            if (dataCount % PageSize > 0)
+            {
+                totalPage++;
+            }
+            return totalPage;
+        }
+        public int GetDataCount(string sqlQuery, Action<SqlParameterCollection> paramAct)
+        {
+            var sqlDataCount = $@"select count(0) from ({sqlQuery}) a ";
+            var dataCount = ExecuteScalar<int>(sqlDataCount, paramAct);
+            return dataCount;
+        }
+        public List<T> ConvertToList<T>(DataTable dataTable) where T : new()
+        {
+            List<T> list = new List<T>();
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                T item = ConvertToObject<T>(row);
+                list.Add(item);
+            }
+
+            return list;
+        }
+        internal T ConvertToObject<T>(DataRow row) where T : new()
+        {
+            T obj = new T();
+
+            foreach (var memberInfo in typeof(T).GetMembers())
+            {
+                if (memberInfo is PropertyInfo property)
+                {
+                    SetPropertyValue(property, obj, row);
+                }
+                else if (memberInfo is FieldInfo field)
+                {
+                    SetFieldValue(field, obj, row);
+                }
+            }
+
+            return obj;
+        }
+
+        private void SetPropertyValue(PropertyInfo property, object obj, DataRow row)
+        {
+            if (row.Table.Columns.Contains(property.Name))
+            {
+                object value = row[property.Name];
+
+                if (value != DBNull.Value)
+                {
+                    if (Nullable.GetUnderlyingType(property.PropertyType) != null)
+                    {
+                        // 处理可空类型属性
+                        property.SetValue(obj, Convert.ChangeType(value, Nullable.GetUnderlyingType(property.PropertyType)));
+                    }
+                    else
+                    {
+                        // 处理非可空类型属性
+                        property.SetValue(obj, Convert.ChangeType(value, property.PropertyType));
+                    }
+                }
+                else
+                {
+                    // 属性值为 DBNull.Value，设置为 null 或默认值
+                    property.SetValue(obj, property.PropertyType.IsValueType ? Activator.CreateInstance(property.PropertyType) : null);
+                }
+            }
+        }
+
+        private void SetFieldValue(FieldInfo field, object obj, DataRow row)
+        {
+            if (row.Table.Columns.Contains(field.Name))
+            {
+                object value = row[field.Name];
+
+                if (value != DBNull.Value)
+                {
+                    if (Nullable.GetUnderlyingType(field.FieldType) != null)
+                    {
+                        // 处理可空类型字段
+                        field.SetValue(obj, Convert.ChangeType(value, Nullable.GetUnderlyingType(field.FieldType)));
+                    }
+                    else
+                    {
+                        // 处理非可空类型字段
+                        field.SetValue(obj, Convert.ChangeType(value, field.FieldType));
+                    }
+                }
+                else
+                {
+                    // 字段值为 DBNull.Value，设置为 null 或默认值
+                    field.SetValue(obj, field.FieldType.IsValueType ? Activator.CreateInstance(field.FieldType) : null);
+                }
+            }
+        }
+
+
+    }
+}
